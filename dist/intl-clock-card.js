9585ce5dec757011fb4237a9b96513e2b2e1fced
jansevera <48588367+jansevera@users.noreply.github.com>
2021-02-16 10:55:23 +0100

    Update intl-clock-card.js
    - bigger border line around the clock
    - bigger numbers in clock with bold font
    (tested on 100px size)

--- dist/intl-clock-card.js
+++ dist/intl-clock-card.js
@@ -109,18 +109,18 @@ class IntlClockCard extends HTMLElement {
             function drawNumbers(ctx, radius) {
                 var ang;
                 var num;
-                ctx.font = radius * 0.15 + "px arial";
+                ctx.font = "700 " + radius * 0.23 + "px arial";
                 ctx.textBaseline = "middle";
                 ctx.textAlign = "center";
                 for (num = 1; num < 13; num++) {
                     ang = num * Math.PI / 6;
                     ctx.fillStyle = theme.numbers ? theme.numbers : getComputedStyle(document.documentElement).getPropertyValue('--primary-text-color');
                     ctx.rotate(ang);
-                    ctx.translate(0, -radius * 0.85);
+                    ctx.translate(0, -radius * 0.78);
                     ctx.rotate(-ang);
-                    ctx.fillText(num.toString(), 0, 0);
+                    ctx.fillText(num.toString(), 0, 0.6);
                     ctx.rotate(ang);
-                    ctx.translate(0, radius * 0.85);
+                    ctx.translate(0, radius * 0.78);
                     ctx.rotate(-ang);
                 }
             }
